0c0aabfdf7e46ff7caaef3fe552f59ac86627e05
hpn33 <hamed.hpn332@gmail.com>
2020-03-09 17:33:47 +0330

    011-Styling the Project Page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 	<header class="flex items-center mb-3 py-4">
-		<div class="flex justify-between items-center w-full">
+		<div class="flex justify-between items-end w-full">
 				
 			<h2 class="text-gray text-sm font-normal">My Projects</h2>
 
@@ -16,13 +16,7 @@
 
 		@forelse($projects as $project)
 			<div class="lg:w-1/3 px-3 pb-6">
-				<div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
-					<h3 class="font-semibold text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
-						<a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
-					</h3>
-
-					<div class="text-gray-text">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
-				</div>
+				@include('projects.card')
 			</div>
 		@empty
 			<div>No Project yet.</div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,10 +2,48 @@
 
 @section('content')
 
-	<h1>{{ $project->title }}</h1>
+	<header class="flex items-center mb-3 py-4">
+		<div class="flex justify-between items-end w-full">
+				
+			<p class="text-gray text-sm font-normal">
+				<a href="/projects" class="text-gray text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+			</p>
 
-	<div>{{ $project->description }}</div>
+			<a href="/projects/create" class="button">New Project</a>	
+		</div>
+		
+	</header>
 
-	<a href="/projects">Go Back</a>
+	<main>
+		<div class="lg:flex -mx-3">
+			<div class="lg:w-3/4 px-3 mb-6">
+				<div class="mb-8">
+					<h2 class="text-gray font-normal text-lg mb-3">Tasks</h2>
+					<!-- tasks -->
+					<div class="card mb-3">some text is here.</div>
+					<div class="card mb-3">some text is here.</div>
+					<div class="card mb-3">some text is here.</div>
+					<div class="card">some text is here.</div>
+				</div>
+
+				<div>
+					<h2 class="text-gray font-normal text-lg mb-3">General Notes</h2>
+					<!-- general notes -->
+					<textarea class="card w-full" style="min-width: 200px">some text is here.</textarea>
+				</div>
+			</div>
+
+			<div class="lg:w-1/4 px-3">
+				@include('projects.card')
+				<!-- div class="card">					
+					<h1>{{ $project->title }}</h1>
+
+					<div>{{ $project->description }}</div>
+
+					<a href="/projects">Go Back</a>
+				</div> -->
+			</div>
+		</div>
+	</main>
 
 @endsection
