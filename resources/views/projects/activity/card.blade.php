cc4c0c239f8b159dde7276442af99ad5fa11484b
hpn33 <hamed.hpn332@gmail.com>
2020-04-10 16:22:00 +0430

    fix

--- resources/views/projects/activity/card.blade.php
+++ resources/views/projects/activity/card.blade.php
@@ -2,10 +2,9 @@
 	<ul class="text-xs list-rest">
 		
 		@foreach($project->activity as $activity)
-
 			<li class="{{ $loop->last ? '' : 'mb-1'}}">
-				@include('project.activity.{$activity->descriptoin}')
-				<span class="text-gray">{{ $activity->create_at->diffForHumans(null, true) }}</span>
+				@include("projects.activity.{$activity->description}")
+				<span class="text-gray-500">{{ $activity->created_at->diffForHumans(null, true) }}</span>
 			</li>
 
 		@endforeach
